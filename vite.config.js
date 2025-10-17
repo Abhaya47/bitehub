dc957290ff1b33844d9ad6bfd3f9d3b43f9ae451
Sahil0718 <sahil.bhattarai46@gmail.com>
2025-10-17 15:15:17 +0545

    created landing page

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite'
+import laravel from 'laravel-vite-plugin'
 
 export default defineConfig({
     plugins: [
@@ -8,6 +7,13 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-        tailwindcss(),
     ],
-});
+    server: {
+        host: '0.0.0.0',
+        port: 5174,
+        hmr: {
+            host: '127.0.0.1',
+            port: 5174,
+        }
+    }
+})  
