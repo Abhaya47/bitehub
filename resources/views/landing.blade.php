534d9d2cb24dc839173e4456524e9cc740f7f40b
Sahil0718 <sahil.bhattarai46@gmail.com>
2025-10-24 13:01:04 +0545

    Completed the landing page

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -3,16 +3,27 @@
 @section('title', 'BiteHub - Discover Restaurants and Delicious Food')
 
 @section('content')
-<div class="relative w-full min-h-screen bg-[#313131]">
-    
-    <!-- Header Component -->
-    @include('components.header')
+    <div class="relative w-full min-h-screen bg-[#313131]">
 
-    <!-- Hero Section Component -->
-    @include('components.hero')
+        <!-- Header Component -->
+        @include('components.header')
 
-    <!-- About Us Section Component -->
-    @include('components.aboutus')
+        <!-- Hero Section Component -->
+        @include('components.hero')
 
-</div>
+        <!-- About Us Section Component -->
+        @include('components.aboutus')
+
+        <!--Service Section Component -->
+        @include('components.service')
+
+        <!--Offers Section Component-->
+        @include('components.offers')
+
+        <!-- Latest Blog Section Component -->
+        @include('components.latestblog')
+
+        <!-- Footer Section Component -->
+        @include('components.footer')
+    </div>
 @endsection
