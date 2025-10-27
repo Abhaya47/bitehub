14c473d6e861d17a4ec5c09d3700390959b7e2a0
Abhaya <abhay.punkid@gmail.com>
2025-10-27 10:33:18 +0545

    small changes

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Livewire\Login;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('landing');
 });
+
+Route::get('/login', Login::class)->name('login');
 
