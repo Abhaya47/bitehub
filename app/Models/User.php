5dc48ea13e704b1eb30fd9d9f8861407cac86a09
Abhaya <abhay.punkid@gmail.com>
2025-10-28 12:26:32 +0545

    just

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use App\Services\UserService;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -72,9 +74,11 @@ class User extends Authenticatable
 
     protected function isUser(): bool{
         $user=Auth::user();
-        if($user->role=="user"){
+        if(($user->role)=="user"){
             return true;
         }
         return false;
     }
+
+
 }
