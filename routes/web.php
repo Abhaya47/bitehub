ba98494bbab61da10697166c65e4b9d25e6ce454
Abhaya <abhay.punkid@gmail.com>
2025-10-31 16:13:17 +0545

    login and register page done

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,19 @@
 <?php
 
 use App\Livewire\Login;
+use App\Livewire\Register;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('landing');
 });
 
+Route::middleware(['web'])->group(function () {
+    Route::get('/login', Login::class)->name('login');
+    Route::get('/register', Register::class)->name('register');
+});
+
+
 Route::get('/home',function(){
     return view('home');
 });
-
-Route::get('/login', Login::class)->name('login');
