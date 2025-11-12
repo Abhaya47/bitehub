f3232cd1761a089bedf34d9b5e9e6eb53c46ed33
Abhaya <abhay.punkid@gmail.com>
2025-11-12 14:46:36 +0545

    working on slider

--- resources/views/livewire/description.blade.php
+++ resources/views/livewire/description.blade.php
@@ -6,7 +6,7 @@
     @include('livewire.description_components.available_offers')
     @include('livewire.description_components.restaurant_menu')
     @include('livewire.description_components.reviews')
-    <livewire:rating-slider :reviews="$restaurant->reviews->toArray()"/>
-    <livewire:latest_reviews :reviews="$restaurant->reviews->toArray()"/>
+    <livewire:rating-slider :reviews="$reviews->toArray()"/>
+    <livewire:latest_reviews :reviews="$reviews->toArray()"/>
     @include('livewire.description_components.footer')
 </div>
